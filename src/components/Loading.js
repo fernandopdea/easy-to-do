e847473af13a90be40437e7fa7649c891ba72d47
Fernando <pupygreen@gmail.com>
2023-01-10 10:01:19 -0300

    first-commit

---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import '../styles/Loading.css';
+
+export default function Loading() {
+  return (
+    <div className="divLoading">
+      <p>Carregando...</p>
+      <div className="loading" />
+    </div>
+  );
+}
